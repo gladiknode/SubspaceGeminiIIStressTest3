f7d8d1a3bbee176c5c980a7caf26794a5dbd5ea8
Arsen44369 <77730159+Arsen44369@users.noreply.github.com>
2022-09-07 15:51:56 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+echo "------------------------------------------------------------------------------------------------"
+echo -e "\e[1;35m██████████████████████████████████████████████████\e[0m"
+echo -e "\e[1;35m█────█─███────█────██───█─██─█─██─█────█────██───█\e[0m"
+echo -e "\e[1;35m█─████─███─██─█─██──██─██─█─██──█─█─██─█─██──█─███\e[0m"
+echo -e "\e[1;35m█─█──█─███────█─██──██─██──███─█──█─██─█─██──█───█\e[0m"
+echo -e "\e[1;35m█─██─█─███─██─█─██──██─██─█─██─██─█─██─█─██──█─███\e[0m"
+echo -e "\e[1;35m█────█───█─██─█────██───█─██─█─██─█────█────██───█\e[0m"
+echo -e "\e[1;35m██████████████████████████████████████████████████\e[0m"
+echo "------------------------------------------------------------------------------------------------"
+
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+	echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+
+sudo apt update && sudo apt install ocl-icd-opencl-dev libopencl-clang-dev libgomp1 -y
+cd $HOME
+rm -rf subspace*
+wget -O subspace-node https://github.com/subspace/subspace/releases/download/gemini-2a-2022-sep-06/subspace-node-ubuntu-x86_64-gemini-2a-2022-sep-06
+wget -O subspace-farmer https://github.com/subspace/subspace/releases/download/gemini-2a-2022-sep-06/subspace-farmer-ubuntu-x86_64-gemini-2a-2022-sep-06
+chmod +x subspace*
+mv subspace* /usr/local/bin/
+
+systemctl stop subspaced subspaced-farmer &>/dev/null
+rm -rf ~/.local/share/subspace*
+
+source ~/.bash_profile
+sleep 1
+
+echo "[Unit]
+Description=Subspace Node
+After=network.target
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/subspace-node --chain gemini-2a --execution wasm --state-pruning archive --validator --name $SUBSPACE_NODENAME
+Restart=on-failure
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target" > $HOME/subspaced.service
+
+
+echo "[Unit]
+Description=Subspaced Farm
+After=network.target
+[Service]
+User=$USER
+Type=simple
+ExecStart=/usr/local/bin/subspace-farmer farm --reward-address $SUBSPACE_WALLET --plot-size 120G
+Restart=on-failure
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target" > $HOME/subspaced-farmer.service
+
+
+mv $HOME/subspaced* /etc/systemd/system/
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable subspaced subspaced-farmer
+sudo systemctl restart subspaced
+sleep 10
+sudo systemctl restart subspaced-farmer
+
+echo "==================================================="
+echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
+if [[ `service subspaced status | grep active` =~ "running" ]]; then
+  echo -e "Your Subspace node \e[32minstalled and works\e[39m!"
+  echo -e "You can check node status by the command \e[7mservice subspaced status\e[0m"
+  echo -e "Press \e[7mQ\e[0m for exit from status menu"
+else
+  echo -e "Your Subspace node \e[31mwas not installed correctly\e[39m, please reinstall."
+fi
+sleep 2
+echo "==================================================="
+echo -e '\n\e[42mCheck farmer status\e[0m\n' && sleep 1
+if [[ `service subspaced-farmer status | grep active` =~ "running" ]]; then
+  echo -e "Your Subspace farmer \e[32minstalled and works\e[39m!"
+  echo -e "You can check node status by the command \e[7mservice subspaced-farmer status\e[0m"
+  echo -e "Press \e[7mQ\e[0m for exit from status menu"
+else
+  echo -e "Your Subspace farmer \e[31mwas not installed correctly\e[39m, please reinstall."
+fi
+echo "------------------------------------------------------------------------------------------------"
